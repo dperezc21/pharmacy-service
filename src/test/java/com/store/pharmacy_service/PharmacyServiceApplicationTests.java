d1d1ebb27c925a711ce46f98890a63ed2d384a7f
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-18 20:02:05 -0500

    feat: initial commit

---
+++ src/test/java/com/store/pharmacy_service/PharmacyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.pharmacy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
