d39a22a0f6cc465ff3d53194d0b213b03d61565a
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-20 11:27:54 -0500

    fix: cors config to service

---
+++ src/main/java/com/store/pharmacy_service/configs/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.store.pharmacy_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todos los endpoints
+                        .allowedOrigins("http://localhost:4200") // frontend Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
